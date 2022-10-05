a2eb6f0902801a2b3142e40fd0a187e76ec7a5f0
jttang <63836507+jttang@users.noreply.github.com>
2022-10-05 22:14:06 +0200

    Add files via upload

--- TandV.m
+++ TandV.m
@@ -1,14 +1,8 @@
 function [T,V] = TandV(x,y,gridx,Q,nu0)
-% This code is a Matlab function for computing \hat{T}_n and \hat{V}_n in
-% "AN RKHS APPROACH FOR PIVOTAL INFERENCE IN FUNCTIONAL LINEAR REGRESSION" by Holger Dette and Jiajun Tang
-% Please download and source the Matlab package Chebfun available at https://www.chebfun.org/
-
-
-
 % function x is measured on the equally spaced grid gridx
 % nrow(x)=n
 
-% Please download and source the Matlab package Chebfun available at https://www.chebfun.org/
+addpath('/Users/jiajunt2/Dropbox/RKHS/rkhs_2021_05_25')
 nuq = nu0 + (1:Q) * (1-nu0) / Q;
 nq = round(size(x,1)*nuq);
 
@@ -22,11 +16,91 @@ xcov = x' * x /size(x,1);
 %%%%%%%% compute eigenfunction
 % num_max = min(ceil(n^(2/5)),ceil(log(n)^2));       %number of candidate eigenfunctions
 
-v = ceil(size(x,1)^0.4);
-num = v;
+v = floor(size(x,1)^0.4);
+vv = v:(2*v);
+CV = zeros(1,v+1);
+
+for kk = 1:length(vv)
+    num = vv(kk);
+    
+    phi = zeros(num,lg);
+    % differential operator
+    L = chebop(0,1);
+    L.op = @(v) diff(v,4);
+    L.lbc = @(v) [diff(v,2);diff(v,3)];       % left boundary condition
+    L.rbc = @(v) [diff(v,2);diff(v,3)];        % right boundary condition
+    
+    B = chebop(0,1);
+    K = @(x,s) F_cheb(x,s,xcov);                 % integral operator
+    B.op = @(v) fred(K,v);   % fred:  Fredholm integral operator (Chebfun@blockFunction)
+    [e,valtemp] = eigs(L,B,num);
+    val= abs(real(diag(valtemp)));
+    
+    
+    % Fetch eigenfunctions of the integro-differential equations
+    % from Chebfun object "e"
+    for k = 1:num
+        fun = chebfun(e(:,k));
+        temp = real(fun(gridx));
+        phi(k,:) = temp'/sqrt(temp*xcov*temp')/dgrid;
+    end
+    
+    Phi = zeros(num, num);
+    for i = 1:num
+        for j = 1:i
+            Phi(i,j) = sum(phi(i,:).*phi(j,:))*dgrid;
+            Phi(i,j) = Phi(j,i);
+        end
+    end
+    Lambda = diag(val);
+    
+    % Five-fold cross-validation to select r
+    for ii = 1:5
+        foldsize = round(size(x,1)/5);
+        foldid = (foldsize *(ii-1)+1):(foldsize*ii);
+        xtrain = xori(setdiff(1:size(x,1),foldid),:);
+        xtest = xori(foldid,:);
+        ytrain = yori(setdiff(1:size(x,1),foldid),:);
+        ytest = yori(foldid,:);
+        
+        % % GCV to select lambda
+        temp = 1e-7;
+        % Layout a grid for choosing lambda
+        lambda_cand = temp*[0.1:0.1:3];
+        gcv = zeros(1, length(lambda_cand));
+        
+        for q = 1:Q
+            n = nq(q);
+            x = xori(1:n,:);
+            y = yori(1:n);
+            Omega = x*phi'*dgrid;
+            for k = 1:length(lambda_cand)
+                tr_Hlambda = 0;
+                Lambda = diag(val);
+                temp2 = (Omega'*Omega+n*lambda_cand(k)*Lambda)\Omega';
+                hatbeta = phi'*temp2*y;
+                haty = x * hatbeta*dgrid;
+                tr_Hlambda = tr_Hlambda+trace(Omega*temp2);
+                gcv(k) = gcv(k)+sum((haty-y).^2)*dgrid/n/(1-tr_Hlambda/n)^2;
+            end
+        end
+        [na,Index]=min(gcv);
+        lambda = lambda_cand(Index);
+        
+        Omega = xtrain*phi'*dgrid;
+        temp2 = (Omega'*Omega+n*lambda*Lambda)\Omega';
+        hatbeta = phi'*temp2*ytrain;
+        hatytest = xtest * hatbeta*dgrid;
+        CV(ii) = CV(ii) + sum((ytest-hatytest).^2)*dgrid;
+    end
+    
+end
+
+[na,Index]=min(CV);
+num = vv(Index);
+
 
 phi = zeros(num,lg);
-
 % differential operator
 L = chebop(0,1);
 L.op = @(v) diff(v,4);
@@ -38,11 +112,6 @@ K = @(x,s) F_cheb(x,s,xcov);                 % integral operator
 B.op = @(v) fred(K,v);   % fred:  Fredholm integral operator (Chebfun@blockFunction)
 [e,valtemp] = eigs(L,B,num);
 val= abs(real(diag(valtemp)));
-
-
-
-% Fetch eigenfunctions of the integro-differential equations
-% from Chebfun object "e"
 for k = 1:num
     fun = chebfun(e(:,k));
     temp = real(fun(gridx));
@@ -58,11 +127,9 @@ for i = 1:num
 end
 Lambda = diag(val);
 
-
-% % GCV
 temp = 1e-7;
-% Layout a grid for choosing lambda
-lambda_cand = temp*(0.1:0.1:3);
+
+lambda_cand = temp*[0.1:0.1:3];
 gcv = zeros(1, length(lambda_cand));
 
 for q = 1:Q
@@ -84,7 +151,7 @@ end
 lambda = lambda_cand(Index);
 
 % Compute T and V
-hatb = zeros(Q,v);
+hatb = zeros(Q,num);
 
 for q = 1:Q
     n = nq(q);
--- bike.m
+++ bike.m
@@ -1,20 +1,7 @@
-% This is the Matlab code used for the real data analysis in
-% "AN RKHS APPROACH FOR PIVOTAL INFERENCE IN FUNCTIONAL LINEAR REGRESSION"
-% by Holger Dette and Jiajun Tang
-%
-% The data analyzed is the Bike-sharing data of Captial Bike Sharing (CBS), obtained from R package ISLR2.
-% References for the Bike-sharing data:
-% Fanaee-T, H. and Gama, J. (2014). Event labeling combining ensemble detectors and background knowledge. Progress in Artificial Intelligence, 2, 113–127.
-% James, G., Witten, D., Hastie, T. and Tibshirani, R. (2021). An Introduction to Statistical Learning: With Applications in R. New York: springer.
-%
-% Please download and source the Matlab package Chebfun available at https://www.chebfun.org/
-
-
-
 clear
 % Get data
 % Bike
-bikeshare = readtable('bikershare.txt');
+bikeshare = readtable('bikers.txt');
 bikeshare.Properties.VariableNames={'season', 'mnth', 'day',...
     'hr', 'holiday', 'weekday', 'workingday', 'weathersit', 'temp', 'atemp',...
     'hum', 'windspeed', 'casual', 'registered', 'bikers'};
@@ -28,25 +15,51 @@ bikes = zeros(1,250);
 t = zeros(100,250);
 
 t = 0:0.01:1;
-temp = zeros(101,250);
-wind = zeros(101,250);
-
 hr0 = cell(1,250);
 temp0 = cell(1,250);
 wind0 = cell(1,250);
 
+% for i = 1:250
+%     day = workday(i);
+%     id = (bikeshare(:,1)==day);
+%     bikes(i) = sum(bikeshare(id,6));
+%     hr = bikeshare(id,2);
+%     temp(hr+1,i) = bikeshare(id,4);
+%     wind(hr+1,i) = bikeshare(id,5);
+% end
+
+d = [];
 for i = 1:250
     day = workday(i);
     id = (bikeshare(:,1)==day);
     bikes(i) = sum(bikeshare(id,6));
-    hr0{i} = bikeshare(id,2)/23;    
+    hr0{i} = bikeshare(id,2)/23;
+
     temp0{i} = bikeshare(id,4);
     wind0{i} = bikeshare(id,5);
+    if(length(temp0{i})<=16 && length(wind0{i})<=16)
+        i = i+1;
+        continue
+    end
+    d = [d i];
+end
+
+temp = zeros(101,length(d));
+wind = zeros(101,length(d));
+
+for ii=1:length(d)
+    i = d(ii);
     f = fit(hr0{i},temp0{i}, 'fourier3');
     temp(:,i)=f(t);
     f = fit(hr0{i},wind0{i}, 'fourier3');
     wind(:,i)=f(t);
 end
+wind = wind(:,d);
+temp = temp(:,d);
+
+wind = (wind-min(wind(:)))/(max(wind(:))-min(wind(:)));
+temp = (temp-min(temp(:)))/(max(temp(:))-min(temp(:)));
+bikes = bikes(d);
 
 
 % Compute the quantiles of the pivotal statistic W
@@ -74,10 +87,44 @@ Wq = quantile(ww, [0.99, 0.95, 0.9]);
 bikes = (bikes-min(bikes))/(max(bikes)-min(bikes));
 
 
+h = figure('Position', [0 0 550 400]);
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(get(gca,'YLabel'),'Rotation',1)
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3)-1.2, pos(4)-0.5])
+histogram(bikes,'FaceColor',[.7 .7 .7])
+saveas(h,'result2/hist.pdf')
+
+
+
+
+
+
+h = figure('Position', [0 0 550 400]);
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(get(gca,'YLabel'),'Rotation',1)
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3)-1.2, pos(4)-0.5])
+plot(0:0.01:1,wind,'color',[.5 .5 .5],'linewidth',0.1)
+saveas(h,'result2/wind.pdf')
+
+
+h = figure('Position', [0 0 550 400]);
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(get(gca,'YLabel'),'Rotation',1)
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3)-1.2, pos(4)-0.5])
+plot(0:0.01:1,temp,'color',[.5 .5 .5],'linewidth',0.1)
+saveas(h,'result2/temp.pdf')
+
+
+bikes = bikes - mean(bikes);
+temp = temp - mean(temp,2);
+wind = wind - mean(wind,2);
 
 
 %% Temperature
-Delta_temp = 1:0.01:1.7;
+Delta_temp = 3.5:0.01:6;
 rj_temp = zeros(3,length(Delta_temp));
 gridx = 0:0.01:1;
 [T,V] = TandV(temp',bikes',gridx,Q,nu0);
@@ -108,9 +155,9 @@ end
 
 
 %% Wind speed
-Delta_wind = 9.7:0.1:11.4;
+Delta_wind = 0.1:0.01:2;
 rj_wind = zeros(3,length(Delta_wind));
- [T,V] = TandV(wind',bikes',gridx,Q,nu0);
+[T,V] = TandV_old(wind',bikes',gridx,Q,nu0);
 for a = 1:3
     for d = 1:length(Delta_wind)
         rj_wind(a,d) = (T>Wq(a)*V+Delta_wind(d));
